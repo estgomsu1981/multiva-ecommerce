3bec7f4dc1e3d83109e8a6b125a823433e5688a8
estgomsu1981 <estgomsu@gmail.com>
2025-07-07 22:22:28 -0600

    feat: Add support for categories

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,29 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import List, Optional
+
+
+# Campos base de un producto
+class ProductBase(BaseModel):
+    nombre: str
+    descripcion: Optional[str] = None
+    especificacion: Optional[str] = None
+    precio: float
+    minimo_compra: Optional[int] = 1
+    descuento: Optional[int] = 0
+    imagen_url: Optional[str] = None
+    category_id: int  # El ID de la categoría a la que pertenece
+
+# Esquema para la creación de un producto
+class ProductCreate(ProductBase):
+    pass
+
+# Esquema para la respuesta de la API (incluye el objeto Category completo)
+class Product(ProductBase):
+    id: int
+    # category: Category  # <-- ¡Aquí está la magia! Anidamos el esquema de Categoría
+
+    class Config:
+        from_attributes = True
 
 # Esquema base con los campos comunes
 class CategoryBase(BaseModel):
@@ -13,7 +37,8 @@ class CategoryCreate(CategoryBase):
 # Esquema para la lectura (lo que la API enviará como respuesta)
 class Category(CategoryBase):
     id: int
-
+    products: List[Product] = []
+    
     class Config:
         from_attributes = True # Permite que Pydantic lea datos desde modelos SQLAlchemy
 
@@ -53,25 +78,4 @@ class UserUpdate(BaseModel):
     categoria_cliente: Optional[str] = None
     tipo_usuario: Optional[str] = None
 
-# Campos base de un producto
-class ProductBase(BaseModel):
-    nombre: str
-    descripcion: Optional[str] = None
-    especificacion: Optional[str] = None
-    precio: float
-    minimo_compra: Optional[int] = 1
-    descuento: Optional[int] = 0
-    imagen_url: Optional[str] = None
-    category_id: int  # El ID de la categoría a la que pertenece
 
-# Esquema para la creación de un producto
-class ProductCreate(ProductBase):
-    pass
-
-# Esquema para la respuesta de la API (incluye el objeto Category completo)
-class Product(ProductBase):
-    id: int
-    category: Category  # <-- ¡Aquí está la magia! Anidamos el esquema de Categoría
-
-    class Config:
-        from_attributes = True
